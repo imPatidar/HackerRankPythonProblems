ef83ae86a47d142cc9d01794ccc07402b8236fb4
impatidar <pankaj_patidar@hotmail.com>
2019-08-15 10:06:27 +0530

    Time Conversion Update

--- timeConversion.py
+++ timeConversion.py
@@ -1,14 +1,22 @@
 import os
 import sys
 
+
 def timeConversion(s):
     # Write your code here.
     hr = s[0:2]
-    if s[-2:] == 'PM':
+    if s[-2:] == 'PM' and s[0:2] == '12':
+        t = '12' + s[2:8]
+    elif s[-2:] == 'PM':
         a = int(hr)
         a = a + 12
         t = str(a) + s[2:8]
-    return t
+    elif s[-2:] == 'AM' and s[0:2] == '12':
+        t = '00' + s[2:8]
+    else:
+        t = s[:8]
+
+    return (t)
 
 
 if __name__ == '__main__':
