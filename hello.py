52a85d7c2b264b066092e3dcab109025e0781fe4
impatidar <pankaj_patidar@hotmail.com>
2019-08-22 11:10:17 +0530

    Jumping on the Clouds HackerRank Problem.

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
 import itertools
-print(list(itertools.permutations("ACHK",2)))
+
+t = set(itertools.permutations([int(x) for x in "5 1 2 4 4 2 4 2 2 5 1 4 3 1 1 1 2 1 4 1".split(" ")], 6))
+c = 0
+print(len(t))
+a = []
+for i in t:
+    a.append(list(i))
+for i in a:
+    i.sort()
+
+
+k = set(a)
+print(len(k))
+
+    
