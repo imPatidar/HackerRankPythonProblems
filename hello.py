43b35a665ed60255a9236c18c2bdfbb31dbe56b0
impatidar <pankaj_patidar@hotmail.com>
2019-08-25 23:48:15 +0530

    Find Runner Up Score in Python one line Code

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
-import itertools
+# import itertools
+#
+# t = set(itertools.permutations([int(x) for x in "5 1 2 4 4 2 4 2 2 5 1 4 3 1 1 1 2 1 4 1".split(" ")], 6))
+#
+# c = 0
+# print(len(t))
+# a = []
+# for i in t:
+#     a.append(list(i))
+# for i in a:
+#     i.sort()
+#
+#
+# k = set(a)
+# print(len(k))
 
-t = set(itertools.permutations([int(x) for x in "5 1 2 4 4 2 4 2 2 5 1 4 3 1 1 1 2 1 4 1".split(" ")], 6))
-
-c = 0
-print(len(t))
-a = []
-for i in t:
-    a.append(list(i))
-for i in a:
-    i.sort()
-
-
-k = set(a)
-print(len(k))
+l=[[x,y] for x in input().split() for y in input().split()]
+print(l)
