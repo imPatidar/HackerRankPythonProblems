f0db9de3332bfbcb75a3e0c1bd55d63351a7d7b0
impatidar <pankaj_patidar@hotmail.com>
2019-08-26 00:26:37 +0530

    Nested List HackerRank Code

---
+++ NestedList.py
@@ -0,0 +1,25 @@
+if __name__ == '__main__':
+    l = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        # l.append(name)
+        # m.append(score)
+        m = []
+        m.append(score)
+        m.append(name)
+        l.append(m)
+
+    t = sorted(l, key=lambda i: (i[1], i[0]), reverse=True)[-2][0]
+    # print(t)
+    q = []
+    for i in l:
+        if i[0] == t:
+            q.append(i[1])
+    print('\n'.join(sorted(q)))
+
+    # t=sorted(m)[-3]
+    # for i in l:
+
+
+
