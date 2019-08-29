11b925b69285214a32e2ffeba8dbdcbd527210b6
impatidar <pankaj_patidar@hotmail.com>
2019-08-29 16:17:04 +0530

    String itertools Combinations Hackerrank Python Problem.

---
+++ venv/itertools.cominations.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import permutations as p
+a,b=input().split()
+a=[char for char in a]
+t=list(p(a,int(b)))
+for i in sorted(t):
+    print(''.join(list(i)))
