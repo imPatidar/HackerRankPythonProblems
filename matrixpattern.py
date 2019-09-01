7cd2bcc3d64a2de8eada29c839527f4306327e50
impatidar <pankaj_patidar@hotmail.com>
2019-09-01 14:28:34 +0530

    Migratory Birds HackerRank Problem.

--- matrixpattern.py
+++ matrixpattern.py
@@ -1,6 +1,6 @@
 a = int(input())
 
-for i in range(2 * a - 1):
-    for j in range(2 * a - 1):
-        print(1 + max(abs(a - i - 1), abs(a - j - 1)),end=" ")
+for i in range(2*a - 1):
+    for j in range(2*a - 1):
+        print(1 + max(abs(a-i-1), abs(a-j-1)), end=" ")
     print()
