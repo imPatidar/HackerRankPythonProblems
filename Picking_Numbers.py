c0180dcf1d748587539568bbc2870c8055cd5516
impatidar <pankaj_patidar@hotmail.com>
2019-09-03 15:55:07 +0530

    Picking Numbers Python Code HackerRank Solution.

---
+++ Picking_Numbers.py
@@ -0,0 +1,25 @@
+import os
+from collections import defaultdict
+
+
+def pickingNumbers(a):
+    d = defaultdict(int)
+    r = 0
+    for val in a:
+        d[val] += 1
+        r = max(r, d[val] + d[val + 1], d[val] + d[val - 1])
+    return r
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
