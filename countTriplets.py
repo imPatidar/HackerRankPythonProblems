a88708adf8baa1c1f6bb04d67a8ca53d287de65e
impatidar <pankaj_patidar@hotmail.com>
2019-09-16 23:48:43 +0530

    Count triplets Hackerrank solution Updated

--- countTriplets.py
+++ countTriplets.py
@@ -1,44 +1,19 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
 from collections import defaultdict
 
 
-# Complete the countTriplets function below.
-def countTriplets(arr, r):
-    m1, m2 = defaultdict(int), defaultdict(int)
+n, r = map(int, input().split())
+arr = list(map(int, input().split()))
+m1, m2 = defaultdict(int), defaultdict(int)
 
-    triplets = 0
+triplets = 0
 
-    for i in reversed(arr):
-        if (i * r) in m2:
-            triplets += m2[i * r]
+for i in reversed(arr):
+    if (i * r) in m2:
+        triplets += m2[i * r]
 
-        if (i * r) in m1:
-            m2[i] += m1[i * r]
+    if (i * r) in m1:
+        m2[i] += m1[i * r]
 
-        m1[i] += 1
+    m1[i] += 1
 
-    return (triplets)
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    nr = input().rstrip().split()
-
-    n = int(nr[0])
-
-    r = int(nr[1])
-
-    arr = list(map(int, input().rstrip().split()))
-
-    ans = countTriplets(arr, r)
-
-    fptr.write(str(ans) + '\n')
-
-    fptr.close()
+print(triplets)
